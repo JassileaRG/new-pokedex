ad28833a1c743c51339435a430c82bd6762701ce
Jassiléa Góis <86447182+JassileaRG@users.noreply.github.com>
2023-01-04 14:59:26 -0300

    Update index.tsx

--- src/pages/telaPrincipal/index.tsx
+++ src/pages/telaPrincipal/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 
 import { Row, Col } from "antd";
@@ -22,18 +22,39 @@ const App = (): JSX.Element => {
       .catch((error) => console.log(error));
   }, []);
 
+  // Armazenar o state da busca
+  const [search, setSearch] = useState('');
+
+  // Realizar a filtragem de itens
+  const filteredItem = useMemo(() => {
+    const lowerSearch = search.toLowerCase();
+
+    return data.filter((item) => item.name.toLowerCase().includes(lowerSearch));
+  }, [search]);
+
   return (
     <div className="container-main">
       <h1>Cabeçalho</h1>
+
+      <div>
+        <h4>Pesquisa</h4>
+        <input
+          type="text"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+      </div>
+
       <div className="container-list">
         <Row gutter={[14, 14]} className="row">
-          {data.map((item) => (
+          {filteredItem.map((item) => (
             <Col span={6} key={item.id}>
               <CardPokemon objeto={item} />
             </Col>
           ))}
         </Row>
       </div>
+
     </div>
   );
 };
